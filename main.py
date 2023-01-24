d90bcb2c42e1139a63bea80bff0478bb3721be78
Jose Manuel Martínez Caro <martinezcaro92@gmail.com>
2023-01-24 14:17:16 +0100

    WIP main.py and map_utils

--- main.py
+++ main.py
@@ -6,13 +6,56 @@ __email__ = "jmmartinez@e-lighthouse.com"
 __status__ = "Working on"
 
 # main.py
+from bcolors import bcolors
 from map_utils import get_circle
-
+import os
+import pandas as pd
+import numpy as np
 
 def main():
     # Your program goes here
-    var = get_circle(1, 2, 3)
-    print(var[1])
+    try:
+        os.system('clear')  #clear the console before start
+        os.system('pip install openpyxl')
+    except Exception as e:
+        print(bcolors.FAIL + 'Error installing the required dependencies' + bcolors.ENDC)
+        print(e)
+
+    try:
+        trasmitting_powers = pd.read_excel('PoF_Simulation_PYTHON/inputParameters.xlsx','TransmittingPowers',index_col=0, header=0)
+        fading_rayleigh_distribution = pd.read_excel('PoF_Simulation_PYTHON/inputParameters.xlsx','FadingRayleighDistribution',index_col=0, header=0)
+        print(trasmitting_powers)
+        print(fading_rayleigh_distribution)
+
+    except Exception as e:
+        print(bcolors.FAIL + 'Error reading from inputParameters.xlsx file' + bcolors.ENDC)
+        print(e)
+
+    try:
+        WeightsTier1 = np.ones((1, fading_rayleigh_distribution.loc['NMacroCells','value']))*trasmitting_powers.loc['PMacroCells','value']
+        WeightsTier2 = np.ones((1, fading_rayleigh_distribution.loc['NFemtoCells','value']))*trasmitting_powers.loc['PFemtoCells','value']
+
+        BaseStations = np.zeros((fading_rayleigh_distribution.loc['NMacroCells','value'] + fading_rayleigh_distribution.loc['NFemtoCells','value'], 3));
+        
+        # Settle Macro cells 
+        BaseStations[0:fading_rayleigh_distribution.loc['NMacroCells','value'],0] = fading_rayleigh_distribution.loc['Maplimit','value'] * np.random.uniform(size=fading_rayleigh_distribution.loc['NMacroCells','value'], low=1, high=fading_rayleigh_distribution.loc['NMacroCells','value'])
+        BaseStations[0:fading_rayleigh_distribution.loc['NMacroCells','value'],1] = fading_rayleigh_distribution.loc['Maplimit','value'] * np.random.uniform(size=fading_rayleigh_distribution.loc['NMacroCells','value'], low=1, high=fading_rayleigh_distribution.loc['NMacroCells','value'])
+        BaseStations[0:fading_rayleigh_distribution.loc['NMacroCells','value'],2] = WeightsTier1
+
+        BaseStations[fading_rayleigh_distribution.loc['NMacroCells','value']:,0] = fading_rayleigh_distribution.loc['Maplimit','value'] * np.random.uniform(size=fading_rayleigh_distribution.loc['NFemtoCells','value'], low=1, high=fading_rayleigh_distribution.loc['NFemtoCells','value'])
+        BaseStations[fading_rayleigh_distribution.loc['NMacroCells','value']:,1] = fading_rayleigh_distribution.loc['Maplimit','value'] * np.random.uniform(size=fading_rayleigh_distribution.loc['NFemtoCells','value'], low=1, high=fading_rayleigh_distribution.loc['NFemtoCells','value'])
+
+        # print(BaseStations)
+
+        Stations = BaseStations.shape
+        Npoints = Stations[0] #actually here
+    except Exception as e:
+        print(bcolors.FAIL + 'Error calculating intermediate variables' + bcolors.ENDC)
+        print(e)
+
+
+
+
 
 
 if __name__ == '__main__':
--- map_utils.py
+++ map_utils.py
@@ -7,18 +7,32 @@ __status__ = "Working on"
 
 from math import pi
 import numpy as np
+from bcolors import bcolors
+import sys
+
 
 def apollonius_circle_path_loss ():
-    print("TBD!")
+    try:
+        print('TBD!')
+    except Exception as e:
+        print(bcolors.FAIL + 'Error in function: ' + sys._getframe( ).f_code.co_name + bcolors.ENDC)
+        print(bcolors.FAIL + 'Error in file: '+ sys._getframe( ).f_code.co_filename + bcolors.ENDC)
+        print(e)
 
-def get_circle(x, y, r):
-    aux = pi/50
-    th = np.arange(0,(2*pi)+aux,aux)
 
-    xunit = r * np.cos(th) + x
-    yunit = r * np.sin(th) + y
+def get_circle(x:float, y:float, r:float):
+    try:
+        _aux = pi/50
+        _th = np.arange(0,(2*pi)+_aux,_aux)
 
-    return xunit, yunit
+        xunit = r * np.cos(_th) + x
+        yunit = r * np.sin(_th) + y
+
+        return xunit, yunit
+    except Exception as e:
+        print(bcolors.FAIL + 'Error in function: ' + sys._getframe( ).f_code.co_name + bcolors.ENDC)
+        print(bcolors.FAIL + 'Error in file: '+ sys._getframe( ).f_code.co_filename + bcolors.ENDC)
+        print(e)
 
 def get_dominance_area():
     print("TBD!")
