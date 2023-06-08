fad0dec494b8dc1f2870721c2c5801e31f631b70
enrique_fer <efernandez@e-lighthouse.com>
2023-06-08 09:26:58 +0200

    simplify run_simulator configuration

--- run_simulator.py
+++ run_simulator.py
@@ -1,7 +1,45 @@
 import logging
 
-from simulator.launch import INPUT_PARAMETERS, CONFIG_PARAMETERS
+#from simulator.launch import INPUT_PARAMETERS, CONFIG_PARAMETERS
 from simulator.launch import execute_simulator
+
+# Default input_parameters. Copy and modify ad-hoc  [Legacy version]
+INPUT_PARAMETERS = {
+        'battery_capacity': 3.3,                # Ah
+        'small_cell_consumption_on': 0.7,       # In Watts
+        'small_cell_consumption_sleep': 0.05,   # In Watts
+        'small_cell_voltage_min': 0.028,        # In mVolts
+        'small_cell_voltage_max': 0.033,        # In mVolts
+        'Maplimit': 1000,                       # Size of Map grid, [dont touch]
+        'Users': 30,
+        'mean_user_speed': 5.5,                 # In m/s
+        'Simulation_Time': 50,                  # In seconds
+        'timeStep': 0.5,                        # In seconds
+        'numberOfLasers': 5,
+        'noise': 2.5e-14,
+        'SMA_WINDOW': 5, 
+        'NMacroCells': 3,
+        'NFemtoCells': 20,
+        'TransmittingPower' : {
+            'PMacroCells': 40,
+            'PFemtoCells': 0.1,
+            'PDevice': 0.1,
+            'MacroCellDownlinkBW': 20e6,
+            'FemtoCellDownlinkBW': 1e9,
+            'alpha_loss': 4.0            
+        }
+    }
+
+CONFIG_PARAMETERS = {
+        'algorithm': 'uc3m',         # Select over: uc3m or elighthouse
+        'use_nice_setup': True,
+        'use_user_list': False,
+        'show_plots': True,
+        'show_live_plots': False,
+        'speed_live_plots': 0.05,
+        'save_output': False,
+        'output_folder': None,
+    }
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(module)s:%(funcName)s: %(message)s', 
                     level=logging.INFO)     # Change level to DEBUG if needed
@@ -30,7 +68,7 @@ if __name__ == "__main__":
     logging.info("Execute simulator...")
     #execute_simulator(config_parameters=simulator_config, custom_parameters=custom_config)
     
-    # Other ways to execute a simulator
+    ## Other ways to execute a simulator
     # Changing the simulator input
     execute_simulator(input_parameters=simulator_input, config_parameters=simulator_config, custom_parameters=custom_config)
     # Setting up the run_name
