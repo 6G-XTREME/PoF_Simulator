409f29077bb40aa3ee2441e099018dc4a043a57e
enrique_fer <efernandez@e-lighthouse.com>
2023-08-07 13:53:33 +0200

    feat: refactor as object oriented

--- start_x.py
+++ start_x.py
@@ -2,11 +2,32 @@ import sys
 from PySide6 import QtWidgets
 from PySide6.QtUiTools import QUiLoader
 
-loader = QUiLoader()    # Set up a Loader object
+class PoF_Simulator_App(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-app = QtWidgets.QApplication(sys.argv)
-centralwidget = loader.load("./ui/simulator.ui", None)
-centralwidget.setWindowTitle("User data")
+        # Load and set up the UI
+        self.loader = QUiLoader()
+        self.centralwidget = self.loader.load("./ui/simulator.ui", None)
+        # Set the central widget for the QMainWindow
+        self.setCentralWidget(self.centralwidget)
+        # Set the window title and initial size to match the loaded UI
+        self.setWindowTitle("User Data")
+        self.setGeometry(100, 100, 800, 600)  # Set window position (x, y) and size (width, height)
+        
+        # Connections
+        self.centralwidget.edit_simulator_input_checkBox.toggled.connect(self.toggle_editing_input_parameters)
+        
+    def toggle_editing_input_parameters(self, state):
+        self.centralwidget.battery_capacity_edit.setReadOnly(state)
+        # Apply style sheet to change appearance when disabled
+        if state:
+            self.centralwidget.battery_capacity_edit.setStyleSheet("QLineEdit:disabled { background-color: #2e2e2e; color: #808080; }")
+        else:
+            self.centralwidget.battery_capacity_edit.setStyleSheet("")  # Clear style sheet
 
-centralwidget.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = PoF_Simulator_App()
+    main_window.show()
+    sys.exit(app.exec())
