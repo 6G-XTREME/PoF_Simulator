f8663acc7bfb544afbe11d604f52d81c3749289d
enrique_fer <efernandez@e-lighthouse.com>
2023-08-07 14:56:56 +0200

    feat: add input parameters

--- start_x.py
+++ start_x.py
@@ -8,23 +8,26 @@ class PoF_Simulator_App(QtWidgets.QMainWindow):
 
         # Load and set up the UI
         self.loader = QUiLoader()
-        self.centralwidget = self.loader.load("./ui/simulator.ui", None)
+        self.ui = self.loader.load("./ui/simulator.ui", None)
+        self.centralwidget = self.ui.findChild(QtWidgets.QWidget, "centralwidget") 
+
         # Set the central widget for the QMainWindow
-        self.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.ui)
         # Set the window title and initial size to match the loaded UI
         self.setWindowTitle("User Data")
-        self.setGeometry(100, 100, 800, 600)  # Set window position (x, y) and size (width, height)
+        # Set the window size to match the specified geometry in Qt Designer
+        self.setGeometry(self.ui.geometry())
         
         # Connections
-        self.centralwidget.edit_simulator_input_checkBox.toggled.connect(self.toggle_editing_input_parameters)
+        self.ui.edit_simulator_input_checkBox.toggled.connect(self.toggle_editing_input_parameters)
         
     def toggle_editing_input_parameters(self, state):
-        self.centralwidget.battery_capacity_edit.setReadOnly(state)
+        self.ui.battery_capacity_edit.setReadOnly(state)
         # Apply style sheet to change appearance when disabled
         if state:
-            self.centralwidget.battery_capacity_edit.setStyleSheet("QLineEdit:disabled { background-color: #2e2e2e; color: #808080; }")
+            self.ui.battery_capacity_edit.setStyleSheet("QLineEdit:disabled { background-color: #2e2e2e; color: #808080; }")
         else:
-            self.centralwidget.battery_capacity_edit.setStyleSheet("")  # Clear style sheet
+            self.ui.battery_capacity_edit.setStyleSheet("")  # Clear style sheet
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
