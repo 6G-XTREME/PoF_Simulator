c37f896f15a567a4cf1ea313e59bf06423adbe63
Antonio Buendia <abuendia@e-lighthouse.com>
2025-04-25 19:16:25 +0200

    Working case 2

--- model/RegionsPlotter.py
+++ model/RegionsPlotter.py
@@ -96,3 +96,96 @@ def standard_plot(
     
     return fig, ax
     
+    
+    
+    
+
+def plot_algorithm_result(
+        regions:list,
+        nodes: list[tuple[float, float, bool, bool, bool]], # (x, y, has_macrocell, has_femtocell, has_hpld)
+        canvas: tuple[plt.Figure, plt.Axes] = None,
+        coverage_area_config: dict = {},
+        macrocell_config: dict = {},
+        femtocell_config: dict = {},
+        hpld_config: dict = {},
+        plot_config: dict = {},
+        extra_plot_functions: list[callable] = [],
+        scaling_factor:int = 1,
+    ):
+    is_own_figure = canvas is None
+    if is_own_figure:
+        fig, ax = plt.subplots(figsize=plot_config.get("figsize", (10, 10)))
+    else:
+        fig, ax = canvas
+    
+    final_cover_area_config = {
+        "alpha": coverage_area_config.get("alpha", 0.3),
+        "edgecolor": coverage_area_config.get("edgecolor", 'black'),
+        "linewidth": coverage_area_config.get("linewidth", 0.5),
+        "linestyle": coverage_area_config.get("linestyle", '--'),
+    }
+    
+    
+    
+    # Paint the regions (bs areas)
+    for i in range(len(regions)-1, -1, -1):
+        region = regions[i]
+        if isinstance(region, Polygon):
+            x, y = region.exterior.coords.xy
+            ax.fill(x, y, **final_cover_area_config)
+        elif isinstance(region, MultiPolygon):
+            for reg in region.geoms:
+                x, y = reg.exterior.coords.xy
+                ax.fill(x, y, **final_cover_area_config)
+        elif isinstance(region, GeometryCollection):
+            for geom in region.geoms:
+                if isinstance(geom, Polygon):
+                    x, y = geom.exterior.coords.xy
+                    ax.fill(x, y, **final_cover_area_config)
+        else:
+            x, y = region.exterior.coords.xy
+            ax.fill(x, y, **final_cover_area_config)
+    
+    
+
+    # node = (x, y, has_macrocell, has_femtocell, has_hpld)
+    #         0  1  2              3              4
+    for node in nodes:
+        # Star if has macrocell, diamond if has femtocell, circle if hosts no cell
+        marker = "*" if node[2] else "d" if node[3] else "o"
+        color = "red" if node[4] else "black"
+        ax.scatter(node[0] * scaling_factor, node[1] * scaling_factor, marker=marker, s=50, color=color)
+        
+    
+        
+        
+    # Extra plot settings, is the figure is own
+    if is_own_figure:
+        
+        # Adjust axis ticks to show real scale values
+        # current_xticks = ax.get_xticks()
+        # current_yticks = ax.get_yticks()
+        # ax.xaxis.set_major_locator(plt.FixedLocator(current_xticks))
+        # ax.yaxis.set_major_locator(plt.FixedLocator(current_yticks))
+        # ax.xaxis.set_major_formatter(plt.FixedFormatter([f'{round(x/scaling_factor, 1)}' for x in current_xticks]))
+        # ax.yaxis.set_major_formatter(plt.FixedFormatter([f'{round(y/scaling_factor, 1)}' for y in current_yticks]))
+        ax.set_xlabel(f"x ({scaling_factor} km)")
+        ax.set_ylabel(f"y ({scaling_factor} km)")
+        
+        # Add title
+        fig.suptitle(plot_config.get("title", "Regions for base stations"))
+        
+        # Add legend for cell types
+        ax.scatter([], [], label="Has Macrocell", marker="*", s=50, color="red")
+        ax.scatter([], [], label="Has Femtocell", marker="d", s=30, color="blue")
+        ax.scatter([], [], label="Has HPLD (size 50)", marker="o", s=50, color="green")
+        ax.scatter([], [], label="Has no HPLD (size 30)", marker="o", s=30, color="black")
+        ax.legend()
+        
+        for extra_plot_function in extra_plot_functions:
+            extra_plot_function(fig, ax)
+        
+        plt.show()
+    
+    return fig, ax
+    
